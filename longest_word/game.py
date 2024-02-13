ee04fd3437c8abb3f228a0690b00d78cb78b7382
Eduards Jelizarovs <eduards.jelizarovs@gmail.com>
2024-02-13 14:48:48 +0000

    Game development with TDD

---
+++ longest_word/game.py
@@ -0,0 +1,35 @@
+# pylint: disable=missing-docstring
+# pylint: disable=too-few-public-methods
+
+import string
+import random
+import requests
+
+class Game:
+    def __init__(self):
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
+
+    def is_valid(self, word):
+        if not word:
+            return False
+        letters = self.grid.copy() # Consume letters from the grid
+        for letter in word:
+            if letter in letters:
+                letters.remove(letter)
+            else:
+                return False
+        return self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
+
+
+game = Game()
+print(game.grid) # --> OQUWRBAZE
+MY_WORD = "BAROQUE"
+game.is_valid(MY_WORD) # --> True
